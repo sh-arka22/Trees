b468c5a66af970e44e8c18bd6e7a98a4c76752fa
arko <arkajyotisaha891@gmail.com>
2022-12-04 09:29:22 +0530

    starting Trees

--- rootToNodePath.cpp
+++ rootToNodePath.cpp
@@ -1,28 +1,23 @@
 vector<TreeNode *> ans;
-bool flag(TreeNode *root, int tar)
-{
+bool flag(TreeNode *root, int tar){
     if (!root)
         return 0;
-    if (root->val == tar)
-    {
+    if (root->val == tar){
         ans.push_back(root);
         return true;
     }
-    if (flag(root->left, tar))
-    {
+    if (flag(root->left, tar)){
         ans.push_back(root);
         return 1;
     }
-    if (flag(root->right, tar))
-    {
+    if (flag(root->right, tar)){
         ans.push_back(root);
         return 1;
     }
     return flag(root->left, tar) or flag(root->right, tar);
 }
 
-vector<TreeNode *> nodeToRootPath(TreeNode *root, int data)
-{
+vector<TreeNode *> nodeToRootPath(TreeNode *root, int data){
     bool call = flag(root, data);
     // reverse(begin(ans), end(ans));
     return ans;
